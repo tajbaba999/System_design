bdc3df562ce0e2b44348184e5eff36d78b370835
tajbaba999 <tajbaba153@gmail.com>
2025-07-12 23:18:52 +0530

    Bookmyshow

--- Bookmyshow/src/controllers/MovieController.java
+++ Bookmyshow/src/controllers/MovieController.java
@@ -3,10 +3,7 @@ package controllers;
 import enums.City;
 import models.movies.Movie;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MovieController {
     Map<City, List<Movie>> cityVsMovies;
@@ -37,7 +34,50 @@ public class MovieController {
         return cityVsMovies.get(city);
     }
 
+
     //REMOVE movie from a particular city, make use of cityVsMovies map
+    public boolean removeMovieFromCity(String moviename, City city){
+        List<Movie> movies = cityVsMovies.get(moviename);
+        if(movies == null) return false;
+
+        Iterator<Movie> iterator = movies.iterator();
+        while (iterator.hasNext()){
+            Movie movie = iterator.next();
+            if(movie.getMoviename().equals(moviename)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Movie getMovieById(int d){
+        for (Movie movie : allMovies){
+            if (movie.getMovieId() == d){
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteMovieById(int id){
+        Movie toDelete = null;
+        for (Movie movie : allMovies){
+            if (movie.getMovieId() == id){
+                toDelete = movie;
+                break;
+            }
+        }
+
+        if(toDelete == null) return false;
+
+        allMovies.remove(toDelete);
+
+        for (List<Movie> movies : cityVsMovies.values()){
+            movies.remove(toDelete);
+        }
+        return  true;
+    }
 
     //UPDATE movie of a particular city, make use of cityVsMovies map
 
