21629008c0f070fc4766d1284a46d12b9ab1e581
tajbaba999 <tajbaba153@gmail.com>
2026-02-23 21:57:40 +0530

    try and catch blocks

---
+++ JavaInterviewQuestions/src/CatchOrderTryBlock.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+import  java.lang.*;
+
+public class CatchOrderTryBlock {
+    public static void main(String[] args) {
+        try {
+
+        }catch (Exception e){
+
+        }
+    }
+}
