0f99c792291c89770cce4cb934bd6a2529ab260e
tajbaba999 <tajbaba153@gmail.com>
2026-02-23 21:58:17 +0530

    Remove ele from arrayList

---
+++ JavaInterviewQuestions/src/ArrayListRemove.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListRemove {
+    public static void main(String[] args) {
+        List<Integer> lst = new ArrayList<>();
+        lst.add(12);
+        lst.add(23);
+        lst.add(34);
+
+        System.out.println(lst);
+        lst.remove(Integer.valueOf(12));
+        System.out.println(lst);
+    }
+}
